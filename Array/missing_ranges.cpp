c0842042bc0878df10a872f04b78de87c77558f5
nafis ahamed <nafisahamed14@gmail.com>
2026-05-05 18:05:57 +0600

    miising

--- Array/missing_ranges.cpp
+++ Array/missing_ranges.cpp
@@ -4,56 +4,98 @@
 // 6 months ago:
 // Google (10) — TikTok (5) — Amazon
 
-// problem Link: Premium Lock https://medium.com/@sanu.here1993/leet-code-163-missing-ranges-f799bdd2ba53
+// problem Link: Premium Lock
+// https://medium.com/@sanu.here1993/leet-code-163-missing-ranges-f799bdd2ba53
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> missingRanges(vector<int>& ranges , int lower, int upper){
-     vector<vector<int>>ans;
-
-     if(ranges.size() == 0){
-         ans.push_back({lower,upper});
-         return ans;
-     }
-
-     if(ranges[0] > lower){
-        ans.push_back({lower, ranges[0] - 1});
-     }
-
-     int size = ranges.size() - 1;
-
-     for(int i = 1; i <= size; i++){
-        if(ranges[i] - ranges[i - 1] != 1){
-           ans.push_back({ranges[i - 1] + 1, ranges[i] - 1});
-        }
-     }
-
-     if(ranges[ranges.size() - 1] < upper){
-        ans.push_back({ranges[ranges.size() - 1] + 1, upper});
-     }
-     
-
-     return ans;
+bool hasValue(vector<int> ranges, int val) {
+  for (int i = 0; i < ranges.size(); i++) {
+    if (ranges[i] == val) {
+      return true;
+    }
+  }
+  return false;
 }
 
-int32_t main(){
+// 0 1 3
+
+// time o(Range * n)
+
+vector<vector<int>> missingRanges(vector<int>& ranges, int lower, int upper) {
+  vector<vector<int>> ans;
+
+  int x = -1;
+  for (int i = lower; i <= upper; i++) {
+    if (!hasValue(ranges, i)) {
+      if (x == -1) {
+        x = i;
+      }
+    } else {
+      if (x != -1) {
+        ans.push_back({x, i - 1});
+        x = -1;
+      }
+    }
+  }
+
+  if (x != -1) {
+    ans.push_back({x, upper});
+  }
+
+  return ans;
+}
+
+// time o(n) space o(k)
+
+// 0 1 3 49
+
+// vector<vector<int>> missingRanges(vector<int>& ranges , int lower, int
+// upper){
+//      vector<vector<int>>ans;
+
+//      if(ranges.size() == 0){
+//          ans.push_back({lower,upper});
+//          return ans;
+//      }
+
+//      if(ranges[0] > lower){
+//         ans.push_back({lower, ranges[0] - 1});
+//      }
+
+//      int size = ranges.size() - 1;
+
+//      for(int i = 1; i <= size; i++){
+//         if(ranges[i] - ranges[i - 1] != 1){
+//            ans.push_back({ranges[i - 1] + 1, ranges[i] - 1});
+//         }
+//      }
+
+//      if(ranges[ranges.size() - 1] < upper){
+//         ans.push_back({ranges[ranges.size() - 1] + 1, upper});
+//      }
+
+//      return ans;
+// }
+
+int32_t main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
-  cout.tie(0); 
-  
-   vector<int> vec ={0, 1, 3, 50, 75};
+  cout.tie(0);
 
-   int lower = 0, upper = 99;
+  vector<int> vec = {0, 1, 3, 50, 75};
 
-   vector<vector<int>>ans = missingRanges(vec,lower,upper);
+  int lower = 0, upper = 99;
 
-   for(auto it : ans){
+  vector<vector<int>> ans = missingRanges(vec, lower, upper);
+
+  for (auto it : ans) {
     cout << "[";
-     for(auto i : it){
-        cout << i << " ";
-     }
-     cout << "]";
-     cout << "\n";
-   }
+    for (auto i : it) {
+      cout << i << " ";
+    }
+    cout << "]";
+    cout << "\n";
+  }
 }
