5d05c0835dc7c26ed98359af435fb942137242db
nafis ahamed <nafisahamed14@gmail.com>
2026-05-11 20:19:13 +0600

    hard problem text_justification

--- strings/Text_justification.cpp
+++ strings/Text_justification.cpp
@@ -17,119 +17,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//    vector<string> fullJustify(vector<string>& words, int maxWidth) {
-
-//         vector<string> ans;
-//         int n = words.size();
-
-//         int i = 0;
-
-//         while(i < n) {
-
-//             int j = i;
-//             int totalChars = 0;
-
-//             while(j < n &&
-//                   totalChars + words[j].size() + (j - i) <= maxWidth) {
-
-//                 totalChars += words[j].size();
-//                 j++;
-//             }
-
-//             int wordCount = j - i;
-//             string line = "";
-
-//             if(j == n || wordCount == 1) {
-
-//                 line += words[i];
-
-//                 for(int k = i + 1; k < j; k++) {
-//                     line += " ";
-//                     line += words[k];
-//                 }
-
-//                 while(line.size() < maxWidth) {
-//                     line += ' ';
-//                 }
-//             }
-//             else {
-
-//                 int gaps = wordCount - 1;
-
-//                 int totalSpaces = maxWidth - totalChars;
-
-//                 int evenSpace = totalSpaces / gaps;
-
-//                 int extra = totalSpaces % gaps;
-
-//                 for(int k = i; k < j - 1; k++) {
-
-//                     line += words[k];
-
-//                     line += string(evenSpace, ' ');
-
-//                     if(extra > 0) {
-//                         line += ' ';
-//                         extra--;
-//                     }
-//                 }
-//                 line += words[j - 1];
-//             }
-
-//             ans.push_back(line);
-
-//             i = j;
-//         }
-
-//         return ans;
-//     }
-
 vector<string> fullJustify(vector<string>& words, int maxWidth) {
-  vector<string> result;
+  int n = words.size();
   int i = 0;
-
-  while (i < words.size()) {
+  vector<string> result;
+  while (i < n) {
     int j = i + 1;
-    int lineLength = words[i].length();
-
-    while (j < words.size() && lineLength + 1 + words[j].length() <= maxWidth) {
-      lineLength += 1 + words[j].length();
+    int lineLength = words[i].size();
+    while (j < n && lineLength + 1 + words[j].size() <= maxWidth) {
+      lineLength += 1 + words[j].size();
       j++;
     }
 
+    int wordCount = j - i;
+    int numSpaces = maxWidth - (lineLength - (wordCount - 1));
     string line = "";
-    int numWords = j - i;
-    int numSpaces = maxWidth - (lineLength - (numWords - 1));
-
-    if (j == words.size() || numWords == 1) {
+    if (wordCount == 1 || j == n) {
       for (int k = i; k < j; k++) {
         line += words[k];
-        if (line.length() < maxWidth) line += " ";
+        if(line.length() < maxWidth){
+            line += " ";
+        }
       }
-      while (line.length() < maxWidth) line += " ";
+      while (line.size() < maxWidth) {
+        line += " ";
+      }
     } else {
-      int spacesBetween = numSpaces / (numWords - 1);
-      int extraSpaces = numSpaces % (numWords - 1);
-
+      int spaceBetween = numSpaces / (wordCount - 1);
+      int extraSpace = numSpaces % (wordCount - 1);
       for (int k = i; k < j; k++) {
         line += words[k];
         if (k < j - 1) {
-          int spacesToApply = spacesBetween;
-
-          if (extraSpaces > 0) {
-            spacesToApply += 1;
-            extraSpaces--;
+          int spaceApply = spaceBetween;
+          if (extraSpace) {
+            spaceApply += 1;
+            extraSpace--;
           }
-
-          line.append(spacesToApply, ' ');
+          while (spaceApply) {
+            line += " ";
+            spaceApply--;
+          }
         }
       }
     }
-
     result.push_back(line);
     i = j;
   }
-
   return result;
 }
 
@@ -137,8 +70,7 @@ int32_t main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
-  vector<string> word = {"This",          "is", "an", "example", "of", "text",
-                         "justification."};
+  vector<string> word = {"This","is","an","example","of","text" "justification."};
 
   int maxWidth = 16;
 
