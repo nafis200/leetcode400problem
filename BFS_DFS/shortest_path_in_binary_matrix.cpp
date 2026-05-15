f08cc44a3aa82c9b9c1b8997726facc12242f632
nafis ahamed <nafisahamed14@gmail.com>
2026-05-15 20:01:40 +0600

    grid shortest optimised

--- BFS_DFS/shortest_path_in_binary_matrix.cpp
+++ BFS_DFS/shortest_path_in_binary_matrix.cpp
@@ -17,7 +17,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// const int inf = 1e9;
+// vector<int> dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+// vector<int> dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+// int n;
+// int m;
+// bool isValid(int i, int j, vector<vector<int>>& grid){
+//    if(i >= 0 && i < n && j >= 0 && j < m && grid[i][j] != 1){
+//        return true;
+//    }
+//    return false;
+// }
+
+// int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+//   n = grid.size();
+//   m = grid[0].size();
+//   vector<vector<int>> grid(n, vector<int>(m, inf));
+
+//   if (grid[0][0] == 1 || grid[n - 1][m - 1] == 1) {
+//     return -1;
+//   }
+//   queue<pair<int,int>>q;
+//   q.push({0, 0});
+//   grid[0][0] = 1;
+//   while(!q.empty()){
+//      auto x = q.front();
+//      q.pop();
+//      int i = x.first;
+//      int j = x.second;
+//      for(int k = 0; k < 8; k++){
+//         int next_i = i + dx[k];
+//         int next_j = j + dy[k];
+//         if(isValid(next_i, next_j, grid)){
+//             if(grid[i][j] + 1 < grid[next_i][next_j]){
+//                 grid[next_i][next_j] = grid[i][j] + 1;
+//                 q.push({next_i, next_j});
+//             }
+//         }
+//      }
+//   }
+//   if(grid[n - 1][m - 1] == inf){
+//      return -1;
+//   }
+//   return grid[n - 1][m - 1];
+// }
+
+// time o(n ^ 2) space o(1)
+
 const int inf = 1e9;
+const int ninf = -1e9;
 vector<int> dx = {-1, -1, -1, 0, 0, 1, 1, 1};
 vector<int> dy = {-1, 0, 1, -1, 1, -1, 0, 1};
 int n;
@@ -32,14 +80,26 @@ bool isValid(int i, int j, vector<vector<int>>& grid){
 int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
   n = grid.size();
   m = grid[0].size();
-  vector<vector<int>> dis(n, vector<int>(m, inf));
+  
+
 
   if (grid[0][0] == 1 || grid[n - 1][m - 1] == 1) {
     return -1;
   }
+
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < m; j++){
+        if(grid[i][j] == 0){
+            grid[i][j] = inf;
+        }
+        else{
+            grid[i][j] = ninf;
+        }
+    }
+  }
+  grid[0][0] = 1;
   queue<pair<int,int>>q;
   q.push({0, 0});
-  dis[0][0] = 1;
   while(!q.empty()){
      auto x = q.front();
      q.pop();
@@ -49,18 +109,19 @@ int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int next_i = i + dx[k];
         int next_j = j + dy[k];
         if(isValid(next_i, next_j, grid)){
-            if(dis[i][j] + 1 < dis[next_i][next_j]){
-                dis[next_i][next_j] = dis[i][j] + 1;
+            if(grid[i][j] + 1 < grid[next_i][next_j]){
+                grid[next_i][next_j] = grid[i][j] + 1;
                 q.push({next_i, next_j});
             }
         }
      }
   }
-  if(dis[n - 1][m - 1] == inf){
+  if(grid[n - 1][m - 1] == inf){
      return -1;
   }
-  return dis[n - 1][m - 1];
+  return grid[n - 1][m - 1];
 }
+
 
 int32_t main() {
   ios_base::sync_with_stdio(0);
