1a3ea0dbe70470afb08c2d31bedc1a0c643c4f2b
nafis ahamed <nafisahamed14@gmail.com>
2026-06-14 18:28:06 +0600

    preorder inorder postorder

---
+++ Binary_Tree/Binary_Tree_PostOrder_Traversal.cpp
@@ -0,0 +1,87 @@
+
+// Meta (3) — Amazon (3) — Google (3) — Bloomberg (2)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class TreeNode {
+ public:
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode(int x) {
+    val = x;
+    left = NULL;
+    right = NULL;
+  }
+};
+
+TreeNode* BuildTree(vector<int>& arr){
+    if(arr.empty()){
+        return NULL;
+    }
+
+    TreeNode* root = new TreeNode(arr[0]);
+
+    queue<TreeNode*> q;
+    q.push(root);
+
+    int i = 1;
+
+    while(!q.empty() && i < arr.size()){
+        TreeNode* cur = q.front();
+        q.pop();
+
+        if(i < arr.size() && arr[i] != -1){
+            cur->left = new TreeNode(arr[i]);
+            q.push(cur->left);
+        }
+        i++;
+
+        if(i < arr.size() && arr[i] != -1){
+            cur->right = new TreeNode(arr[i]);
+            q.push(cur->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+void preorder(TreeNode* root) {
+    if (root == NULL) return;
+
+    cout << root->val << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+vector<int>ans;
+
+vector<int> postorderTraversal(TreeNode* root) {
+    if(root == NULL){
+        return {};
+    }        
+
+    postorderTraversal(root->left);
+    postorderTraversal(root->right);
+    ans.push_back(root->val);
+    return ans;
+ }
+
+
+
+
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    vector<int>arr = {1,2,3,4,5,-1,8,-1,-1,6,7,9};
+    TreeNode* root = BuildTree(arr);
+    
+    vector<int>result = postorderTraversal(root);
+
+    for(auto it : result){
+        cout << it << " ";
+    }
+}
